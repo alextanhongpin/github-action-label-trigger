bb4701da1f1a39909b94cc14567d7652c34a3353
alextanhongpin <alextan220990@gmail.com>
2023-03-31 00:05:39 +0800

    fix: update schema

--- schemas/user.sql
+++ schemas/user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
 
   `password` varchar(255) NULL,
